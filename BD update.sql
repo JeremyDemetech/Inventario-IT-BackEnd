4b193ad9793e526b5cd15e30777b11bee49a53e7
JeremyDemetech <jeremy.berrios@demetech.us>
2026-04-10 15:46:24 -0600

    cambio en BD

---
+++ BD update.sql
@@ -0,0 +1,20 @@
+
+--CodUser	int	not null
+--Username	nvarchar(50)	null
+--Name	nvarchar(50)	null
+--email	nvarchar(50)	null
+--Password	nvarchar(50)	null
+--status	int	null
+--History	nvarchar(50)	null
+--UserGroup	nvarchar(50)	null
+--ConectedStatus	smallint	null
+--Location	nvarchar(50)	null
+--Imagen	nvarchar(max)	null
+--initial	nvarchar(max)	null
+--IdDept	int	null
+--Skin	varchar(max)	not null
+--ImageUser	image	null
+
+
+SELECT * FROM dbo.[User]
+
